f54333bf2f2e0db2d5b5a3c6154d3feecfb9376b
roy lee <roylee@umich.edu>
2023-06-03 16:41:09 +0900

    perform clustering (DBSCAN, KMeans)

--- crawler/team_crawler_from_fbref.py
+++ crawler/team_crawler_from_fbref.py
@@ -5,11 +5,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-API_DELAY_TERM = 5
+API_DELAY_TERM = 10
 
 
 def web_driver():
     driver = webdriver.Chrome('chromedriver')
+    driver.maximize_window()
     return driver
 
 
@@ -38,11 +39,11 @@ def get_text_strip(element):
 def crawling_stat(team_url):
     team_driver = web_driver()
     team_driver.get(team_url)
-    # time.sleep(API_DELAY_TERM)
-    WebDriverWait(team_driver, 10).until(is_ready)
+    time.sleep(API_DELAY_TERM)
+    WebDriverWait(team_driver, 20).until(is_ready)
 
     # make pandas dataframe
-    summary_columns = ['Team', '# Pl', 'Age', 'Poss','Gls', 'Ast', 'G-PK', 'PK', 'PKatt', 'CrdY', 'CrdR',
+    summary_columns = ['Team', '# Pl', 'Age', 'Poss', 'Gls', 'Ast', 'G-PK', 'PK', 'PKatt', 'CrdY', 'CrdR',
                        'xG', 'npxG', 'xAG', 'npxG+xAG', 'PrgC', 'PrgP']
 
     opponent_columns = ['Team', 'Gls_Conceded', 'Ast_Conceded', 'G-PK_Conceded', 'PK_Conceded', 'PKatt_Conceded',\
@@ -71,25 +72,28 @@ def crawling_stat(team_url):
         team_dict['Poss'] = get_text_strip(td_elements[2])
         team_dict['Gls'] = get_text_strip(td_elements[7])
         team_dict['Ast'] = get_text_strip(td_elements[8])
-        team_dict['G-PK'] = get_text_strip(td_elements[9])
-        team_dict['PK'] = get_text_strip(td_elements[10])
-        team_dict['PKatt'] = get_text_strip(td_elements[11])
-        team_dict['CrdY'] = get_text_strip(td_elements[12])
-        team_dict['CrdR'] = get_text_strip(td_elements[13])
-        team_dict['xG'] = get_text_strip(td_elements[14])
-        team_dict['npxG'] = get_text_strip(td_elements[15])
-        team_dict['xAG'] = get_text_strip(td_elements[16])
-        team_dict['npxG+xAG'] = get_text_strip(td_elements[17])
-        team_dict['PrgC'] = get_text_strip(td_elements[18])
-        team_dict['PrgP'] = get_text_strip(td_elements[19])
+        team_dict['G-PK'] = get_text_strip(td_elements[10])
+        team_dict['PK'] = get_text_strip(td_elements[11])
+        team_dict['PKatt'] = get_text_strip(td_elements[12])
+        team_dict['CrdY'] = get_text_strip(td_elements[13])
+        team_dict['CrdR'] = get_text_strip(td_elements[14])
+        team_dict['xG'] = get_text_strip(td_elements[15])
+        team_dict['npxG'] = get_text_strip(td_elements[16])
+        team_dict['xAG'] = get_text_strip(td_elements[17])
+        team_dict['npxG+xAG'] = get_text_strip(td_elements[18])
+        team_dict['PrgC'] = get_text_strip(td_elements[19])
+        team_dict['PrgP'] = get_text_strip(td_elements[20])
 
         print(team_dict)
         # add disctionary date to dataframe
         team_df_summary.loc[len(team_df_summary)] = team_dict
 
     # to get opponent's stats
-    opponent_info = team_driver.find_element(By.XPATH, "/html/body/div[2]/div[6]/div[2]/div[3]/div[2]")
-    opponent_info.find_element(By.CSS_SELECTOR, "a").click()
+    # opponent_info = team_driver.find_element(By.XPATH, "/html/body/div[2]/div[6]/div[2]/div[3]/div[2]")
+    # opponent_link = opponent_info.find_element(By.CSS_SELECTOR, "a")
+    opponent_link = team_driver.find_element(By.XPATH, "/html/body/div[2]/div[6]/div[2]/div[3]/div[2]/a")
+    print(opponent_link)
+    opponent_link.click()
     time.sleep(API_DELAY_TERM)
 
     opponent_elements = team_driver.find_element(By.ID, 'stats_squads_standard_against')\
@@ -104,17 +108,17 @@ def crawling_stat(team_url):
 
         team_dict['Gls_Conceded'] = get_text_strip(td_elements[7])
         team_dict['Ast_Conceded'] = get_text_strip(td_elements[8])
-        team_dict['G-PK_Conceded'] = get_text_strip(td_elements[9])
-        team_dict['PK_Conceded'] = get_text_strip(td_elements[10])
-        team_dict['PKatt_Conceded'] = get_text_strip(td_elements[11])
-        team_dict['CrdY_Conceded'] = get_text_strip(td_elements[12])
-        team_dict['CrdR_Conceded'] = get_text_strip(td_elements[13])
-        team_dict['xG_Conceded'] = get_text_strip(td_elements[14])
-        team_dict['npxG_Conceded'] = get_text_strip(td_elements[15])
-        team_dict['xAG_Conceded'] = get_text_strip(td_elements[16])
-        team_dict['npxG+xAG_Conceded'] = get_text_strip(td_elements[17])
-        team_dict['PrgC_Conceded'] = get_text_strip(td_elements[18])
-        team_dict['PrgP_Conceded'] = get_text_strip(td_elements[19])
+        team_dict['G-PK_Conceded'] = get_text_strip(td_elements[10])
+        team_dict['PK_Conceded'] = get_text_strip(td_elements[11])
+        team_dict['PKatt_Conceded'] = get_text_strip(td_elements[12])
+        team_dict['CrdY_Conceded'] = get_text_strip(td_elements[13])
+        team_dict['CrdR_Conceded'] = get_text_strip(td_elements[14])
+        team_dict['xG_Conceded'] = get_text_strip(td_elements[15])
+        team_dict['npxG_Conceded'] = get_text_strip(td_elements[16])
+        team_dict['xAG_Conceded'] = get_text_strip(td_elements[17])
+        team_dict['npxG+xAG_Conceded'] = get_text_strip(td_elements[18])
+        team_dict['PrgC_Conceded'] = get_text_strip(td_elements[19])
+        team_dict['PrgP_Conceded'] = get_text_strip(td_elements[20])
 
         print(team_dict)
         # add disctionary date to dataframe
@@ -141,8 +145,9 @@ def save_team(league):
 
     """
 
-    years = ['2016-2017', '2017-2018', '2018-2019', '2019-2020', '2020-2021', '2021-2022']
+    years = ['2019-2020', '2020-2021', '2021-2022']
 
+    # years = ['2016-2017']
     # get team dataframe function
     def get_team_df(year):
         stat_url = f'https://fbref.com/en/comps/9/{year}/stats/{year}-Premier-League-Stats'
@@ -152,7 +157,7 @@ def save_team(league):
     for year in years:
         print(f"--- start collecting data : {year}")
         data = get_team_df(year)
-        data.to_csv(f"../datasets/teams/fbref.com/team_stats_{year}.csv")
+        data.to_csv(f"../datasets/raw/unsupervised_learning/teams/fbref.com/team_stats_{year}.csv")
         print(f"--- finished data collection for {year} -" * 35)
 
 
